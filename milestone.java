c4ea31f1520fa30f5b4090e29c750cdaf32e3fa8
veena <veenapagati.xworkz@gmail.com>
2022-09-15 08:22:44 +0530

    mile stone

---
+++ milestone.java
@@ -0,0 +1,62 @@
+import javax.swing.undo.StateEditable;
+
+class milestone {
+    static void ShowTheResponsibility() {
+        system.out.println("start the show responsibility");
+        int age;
+        age = 10;
+        if (age == 10) {
+            system.out.println("going to school");
+        }
+        if (age == 16) {
+            System.out.println("going to college");
+            return;
+        }
+        System.out.println("its time to vote");
+        System.out.println("may get driving licence");
+        if (gender == f) {
+            System.out.println("girls can marry");
+        } else {
+            if (gender == m)
+                System.out.println("boys cant marry");
+        }
+        if (age == 21) {
+            System.out.println("boys can marry");
+            return;
+        }
+        if (age == 22 || age == 22 || age == 23) {
+            sout("they may get graduated");
+        }
+        if (age == 23 || age == 25) {
+            System.out.println("they must get a job");
+            return;
+        }
+        if (age == 28) {
+            sout("they may get a kids");
+            return;
+        }
+        if (age == 30) {
+            System.out.println("having another kid");
+        }
+        if (age == 45) {
+            System.out.println("kids future");
+        }
+        if (age == 50) {
+            System.out.println("kids education");
+            return;
+        }
+        if (age == 58) {
+            System.out.println("retired");
+            return;
+        }
+        if (age == 60) {
+            System.out.println("grand kids");
+        }
+        if (age == 70) {
+            System.out.println("RIP");
+
+        }
+
+    }
+
+}
---
+++ milestoneRunner.java
@@ -0,0 +1,6 @@
+class MilestoneRunner {
+    public static void main(String[] args)
+    {
+        milestone.ShowTheResponsibility;
+    }
+}
